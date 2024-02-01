34c008d04bdae40241c2b117161798c3627bef40
tuki <>
2024-02-01 21:36:33 +0900

    採用か不採用かによって鳴らす音を変える

--- noAdopted/content.js
+++ noAdopted/content.js
@@ -1,5 +1,6 @@
-let Tmusic = '';
-let Fmusic = '';
+let Tmusic = ''; //採用
+let Fmusic = ''; //不採用
+let Adopted = 0; //判定用
 
 // 嬉しい曲
 var musicURL = chrome.runtime.getURL('music/vinculum.mp3');
@@ -10,26 +11,25 @@ var musicURL2 = chrome.runtime.getURL('music/hotaru-piano.mp3');
 Fmusic = new Audio(musicURL2);
 
 function f(n) {
-  let noAdopted = 0;
   let cs = n.childNodes;
   for (let i = 0; i < cs.length; i++) {
     let c = cs[i];
     if (c.nodeType == Node.TEXT_NODE) {
       c.textContent = c.textContent.replace(/。/g, 'にゃん。');
       if (/添いかねる|残念|見送/.test(c.textContent)) {
-        noAdopted = 1;
+        Adopted = 1;
       }
     } else {
       f(c);
     }
   }
-  return noAdopted;
+  return Adopted;
 }
 
 let noAdopted = f(document.body); // document.body は HTML の文章全体を示す要素
 
 if (window.confirm('音鳴らすにゃん？')) {
-  if (noAdopted) {
+  if (noAdopted == 1) {
     Fmusic.play();
   } else {
     Tmusic.play();
